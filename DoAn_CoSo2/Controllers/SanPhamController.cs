e1dfeb5562b181ef47e6d77d1ec427acf425c7fc
Minh Trần <tcminh1108@gmail.com>
2022-05-04 20:04:20 +0700

    Add project files.

---
+++ DoAn_CoSo2/Controllers/SanPhamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_CoSo2.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult Index_SanPham()
+        {
+            return View();
+        }
+    }
+}
---
+++ DoAn_CoSo2/Controllers/TrangChuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_CoSo2.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        public ActionResult Index_TrangChu()
+        {
+            return View();
+        }
+    }
+}
